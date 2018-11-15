37bc16eab6ccad967693e848b6a930b7b5196d51
Till Krüss <till@kruss.io>
2018-11-14 16:04:29 -0800

    switch to spaces

--- includes/dismiss-notice.js
+++ includes/dismiss-notice.js
@@ -1,12 +1,12 @@
 ( function ( $ ) {
-	$( function () {
-		$( ".notice[data-dismissible] .notice-dismiss" ).click(function (event) {
-			$.post( ajaxurl, {
-				notice: $( this ).parent().attr( "data-dismissible" ),
-				action: "eae_dismiss_notice",
-			} );
+    $( function () {
+        $( ".notice[data-dismissible] .notice-dismiss" ).click(function (event) {
+            $.post( ajaxurl, {
+                notice: $( this ).parent().attr( "data-dismissible" ),
+                action: "eae_dismiss_notice",
+            } );
 
-			event.preventDefault();
-		} );
-	} );
+            event.preventDefault();
+        } );
+    } );
 } ( jQuery ) );
