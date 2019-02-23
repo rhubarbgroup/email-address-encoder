62c8fb1972f840c93c4ac29f2486b92af0020079
Till Krüss <till@kruss.io>
2019-02-23 10:23:22 -0800

    sync text

--- includes/ui.php
+++ includes/ui.php
@@ -67,7 +67,7 @@
                             <br>
                             <label>
                                 <input type="radio" name="eae_search_in" value="filters" disabled>
-                                <?php _e( 'Full page scan', 'email-address-encoder' ); ?>
+                                <?php _e( 'Full-page scanner', 'email-address-encoder' ); ?>
                                 (<a target="_blank" rel="noopener" href="https://encoder.till.im/download?utm_source=wp-plugin&utm_medium=setting"><?php _e( 'Premium only', 'email-address-encoder' ); ?></a>)
                                 <p class="description">
                                     <small><?php _e( 'Protects all email addresses on your site.', 'email-address-encoder' ); ?></small>
@@ -76,7 +76,7 @@
                             <br>
                             <label>
                                 <input type="radio" name="eae_search_in" value="void" <?php checked( 'void', get_option( 'eae_search_in' ) ); ?>>
-                                <?php _e( 'Nothing', 'email-address-encoder' ); ?>
+                                <?php _e( 'Nothing, don’t do anything', 'email-address-encoder' ); ?>
                                 <p class="description">
                                     <small><?php _e( 'Turns off email protection.', 'email-address-encoder' ); ?></small>
                                 </p>
