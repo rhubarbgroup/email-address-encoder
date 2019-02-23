d8660bc18abb5d951caf623839f25ce8446ab5df
Till Krüss <till@kruss.io>
2019-02-23 13:24:37 -0800

    spacing

--- includes/dismiss-notice.js
+++ includes/dismiss-notice.js
@@ -1,6 +1,6 @@
 ( function ( $ ) {
     $( function () {
-        $( ".notice[data-dismissible] .notice-dismiss" ).click(function (event) {
+        $( ".notice[data-dismissible] .notice-dismiss" ).click( function ( event ) {
             $.post( ajaxurl, {
                 notice: $( this ).parent().attr( "data-dismissible" ),
                 action: "eae_dismiss_notice",
